12c14e1dce6274298b62711daf56b7f9415de6b2
Sławomir Grzebyk <grzebyk.slawomir@gmail.com>
2024-11-23 14:38:13 +0100

    Change Next.js version

---
+++ next.config.js
@@ -0,0 +1,18 @@
+
+
+const nextConfig = {
+  // reactStrictMode: false, // Opcjonalnie możesz to ustawić w zależności od potrzeb
+  // devIndicators: {
+  //   buildActivity: false, // Wyłącza wskaźnik aktywności kompilacji w rogu ekranu
+  // },
+  // webpack: (config, { dev }) => {
+  //   if (dev) {
+  //     config.devServer = {
+  //       overlay: false, // Wyłącza czerwony popup błędów
+  //     };
+  //   }
+  //   return config;
+  // },
+};
+
+export default nextConfig;
