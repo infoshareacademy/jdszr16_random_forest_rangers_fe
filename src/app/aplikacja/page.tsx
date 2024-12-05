5742de25af4a9dd557bdc22e876de72e5bcb6d97
Sławomir Grzebyk <grzebyk.slawomir@gmail.com>
2024-12-05 16:19:35 +0100

    format with prettier

--- src/app/aplikacja/page.tsx
+++ src/app/aplikacja/page.tsx
@@ -1,32 +1,25 @@
-'use client'
+"use client";
 
-
-import React, {useState, useEffect} from 'react';
-import type { FormProps } from 'antd';
-import { Button, Form, Input, Divider } from 'antd';
-import MoreInfo from './moreInfo';
+import React, { useState, useEffect } from "react";
+import type { FormProps } from "antd";
+import { Button, Form, Input, Divider } from "antd";
+import MoreInfo from "./moreInfo";
 // import styles from "../page.module.css";
-
-
 
 type FieldType = {
   holesterol?: string;
   cukier?: string;
 };
 
-
-const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-  console.log('Failed:', errorInfo);
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+  console.log("Failed:", errorInfo);
 };
 
-
 export default function Home() {
+  const initialValues = { holesterol: "holesterol", cukier: "cukier" };
 
-  const initialValues = { holesterol: "holesterol" , cukier: "cukier" };
-
-
-const [isClient, setIsClient] = useState(false);
- // const [resData, setResData] = useState('')
+  const [isClient, setIsClient] = useState(false);
+  // const [resData, setResData] = useState('')
 
   // Ustawienie renderowania tylko na kliencie
   useEffect(() => {
@@ -35,77 +28,70 @@ const [isClient, setIsClient] = useState(false);
 
   if (!isClient) return null; // Renderuj tylko po stronie klienta
 
-    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+  const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
+    console.log("values", values);
 
-        console.log('values', values)
-
-  // try {
-  //   const response = await fetch('https://jdszr16-random-forest-rangers-be.onrender.com/test', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //      body: JSON.stringify(values),
-  //   });
-  //
-  //   if (!response.ok) {
-  //     throw new Error('Network response was not ok');
-  //   }
-  //
-  //   const data = await response.json();
-  //
-  //   setResData(data.message)
-  //
-  // } catch (error) {
-  //   console.error('Error:', error);
-  // }
-
-};
-
+    // try {
+    //   const response = await fetch('https://jdszr16-random-forest-rangers-be.onrender.com/test', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //      body: JSON.stringify(values),
+    //   });
+    //
+    //   if (!response.ok) {
+    //     throw new Error('Network response was not ok');
+    //   }
+    //
+    //   const data = await response.json();
+    //
+    //   setResData(data.message)
+    //
+    // } catch (error) {
+    //   console.error('Error:', error);
+    // }
+  };
 
   return (
+    <div>
+      <Form
+        name="basic"
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 400 }}
+        initialValues={initialValues}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          label="Holesterol"
+          name="holesterol"
+          rules={[{ required: true, message: "Please input your username!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <div>
-         <Form
-    name="basic"
-    labelCol={{ span: 6 }}
-     wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 400 }}
-    initialValues={initialValues}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Holesterol"
-      name="holesterol"
-      rules={[{ required: true, message: 'Please input your username!' }]}
-    >
-      <Input />
-    </Form.Item>
+        <Form.Item<FieldType>
+          label="Cukier"
+          name="cukier"
+          rules={[{ required: true, message: "Wprowadz cukier" }]}
+        >
+          <Input />
+        </Form.Item>
 
- <Form.Item<FieldType>
-      label="Cukier"
-      name="cukier"
-      rules={[{ required: true, message: 'Wprowadz cukier' }]}
-    >
-      <Input />
-    </Form.Item>
+        <Form.Item label={null}>
+          <Button type="primary" htmlType="submit" disabled>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      <Divider orientation="left" plain>
+        Dodatkowe informacje o chorobie
+      </Divider>
 
-
-    <Form.Item label={null}>
-      <Button type="primary" htmlType="submit" disabled>
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-          <Divider orientation="left" plain>
-      Dodatkowe informacje o chorobie
-    </Divider>
-
-          <MoreInfo />
-
-</div>
-
+      <MoreInfo />
+    </div>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,68 +1,58 @@
-'use client'
+"use client";
 // import type {Metadata} from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import {Layout, Menu} from 'antd';
-import Link from 'next/link';
-import {usePathname} from 'next/navigation';
+import { Layout, Menu } from "antd";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "@/app/page.module.css";
 
-
-const {Header } = Layout;
-
+const { Header } = Layout;
 
 const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
-
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
 
-    const pathname = usePathname();
+  const items = [
+    {
+      key: "/",
+      label: <Link href="/">Home</Link>,
+    },
+    {
+      key: "/aplikacja",
+      label: <Link href="/aplikacja">Aplikacja</Link>,
+    },
+  ];
 
-    const items = [
-        {
-            key: '/',
-            label: <Link href="/">Home</Link>,
-        },
-        {
-            key: '/aplikacja',
-            label: <Link href="/aplikacja">Aplikacja</Link>,
-        },
-    ];
-
-    return (
-        <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
-
-        <Header style={{display: 'flex', alignItems: 'center'}}>
-            <div className="demo-logo"/>
-            <Menu
-                theme="dark"
-                mode="horizontal"
-                selectedKeys={[pathname === '/' ? '/' : pathname]}
-                items={items}
-                style={{flex: 1, minWidth: 0}}
-            />
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header style={{ display: "flex", alignItems: "center" }}>
+          <div className="demo-logo" />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            selectedKeys={[pathname === "/" ? "/" : pathname]}
+            items={items}
+            style={{ flex: 1, minWidth: 0 }}
+          />
         </Header>
-        <div className={styles.page}>
-
-        {children}
-
-    </div>
-        </body>
-        </html>
-    );
+        <div className={styles.page}>{children}</div>
+      </body>
+    </html>
+  );
 }
-
